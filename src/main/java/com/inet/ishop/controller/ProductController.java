114765e612cad3729827c86831c246463a82cc8f
Sroh Bunlin <srohbunlin@Srohs-MacBook-Pro.local>
2026-06-17 22:10:12 +0700

    new update

--- src/main/java/com/inet/ishop/controller/ProductController.java
+++ src/main/java/com/inet/ishop/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://i-knet.com", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
