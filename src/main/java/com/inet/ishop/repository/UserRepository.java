886f580850551322398a3637b003d9ae5459e5b6
Sroh Bunlin <srohbunlin@Srohs-MacBook-Pro.local>
2026-06-28 16:45:53 +0700

    new update

--- src/main/java/com/inet/ishop/repository/UserRepository.java
+++ src/main/java/com/inet/ishop/repository/UserRepository.java
@@ -9,11 +9,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // ស្វែងរកអ្នកប្រើប្រាស់តាមរយៈ Username (ប្រើសម្រាប់ Authentication)
-    static Optional<User> findByUsername(String name) {
-        return Optional.empty();
-    }
-
-    static void Save() {
+    static Optional<User> findByUsername(String username) {
+        return null;
     }
 
 }
